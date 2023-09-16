8d07ea16eb550358c6a8e338bf8101a2a9f2bee9
GetPsyched <priyanshutr@proton.me>
2023-09-17 01:44:51 +0530

    feat: add support for aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -5,10 +5,11 @@
     let
       forAllSystems = function:
         nixpkgs.lib.genAttrs [
-          "x86_64-linux"
+          "aarch64-darwin"
           "aarch64-linux"
           "i686-linux"
           "x86_64-darwin"
+          "x86_64-linux"
         ]
           (system: function nixpkgs.legacyPackages.${system});
     in
