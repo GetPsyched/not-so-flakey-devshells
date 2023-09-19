0ce02b02fe68d65ca41e7d130450b97b4a2244ae
GetPsyched <priyanshutr@proton.me>
2023-09-19 08:41:21 +0530

    python: init

---
+++ python/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Python environment";
+
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "aarch64-darwin"
+          "aarch64-linux"
+          "i686-linux"
+          "x86_64-darwin"
+          "x86_64-linux"
+        ]
+          (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = with pkgs; symlinkJoin {
+          name = "default";
+          paths = [ python311 nixpkgs-fmt ];
+        };
+        vscode = (pkgs.vscode-with-extensions.override {
+          vscode = pkgs.vscodium;
+          vscodeExtensions = with pkgs.vscode-extensions; [
+            jnoortheen.nix-ide
+            ms-pyright.pyright
+            ms-python.python
+          ];
+        });
+      });
+    };
+}
