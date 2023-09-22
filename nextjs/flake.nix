5e978a948403dec64cfe8c0b8bb7ad99cda50657
GetPsyched <priyanshutr@proton.me>
2023-09-22 16:31:41 +0530

    nextjs: init

---
+++ nextjs/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "NextJS environment";
+
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "aarch64-darwin"
+          "aarch64-linux"
+          "i686-linux"
+          "x86_64-darwin"
+          "x86_64-linux"
+        ]
+          (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = with pkgs; symlinkJoin {
+          name = "default";
+          paths = [ nodePackages.pnpm nixpkgs-fmt ];
+        };
+        vscode = (pkgs.vscode-with-extensions.override {
+          vscode = pkgs.vscodium;
+          vscodeExtensions = with pkgs.vscode-extensions; [
+            esbenp.prettier-vscode
+            jnoortheen.nix-ide
+          ];
+        });
+      });
+    };
+}
