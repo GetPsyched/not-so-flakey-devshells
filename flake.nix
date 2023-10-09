b693f95d2bb69b24931b47edfa8bb721c3604c18
GetPsyched <priyanshutr@proton.me>
2023-10-09 14:35:42 +0530

    feat: add yaml extension for GH workflows

--- flake.nix
+++ flake.nix
@@ -16,7 +16,16 @@
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with nix-env-pkgs; [
           default
-          vscode
+          (vscode.override {
+            vscodeExtensions = with pkgs.vscode-extensions; [
+              jnoortheen.nix-ide
+            ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+              name = "vscode-yaml";
+              publisher = "redhat";
+              version = "1.14.0";
+              sha256 = "sha256-hCRyDA6oZF7hJv0YmbNG3S2XPtNbyxX1j3qL1ixOnF8=";
+            }];
+          })
           pkgs.just
         ];
       };
