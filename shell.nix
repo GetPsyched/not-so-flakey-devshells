7f05aeab82be6fa8769c1e355f025bc64c9f017d
GetPsyched <priyanshu@getpsyched.dev>
2023-11-07 17:14:59 +0530

    refactor: move to shell.nix

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs.buildPackages; [
+    just
+    nixpkgs-fmt
+    (callPackage ./packages/vscode.nix {
+      extensions = vscode-utils.extensionsFromVscodeMarketplace [{
+        name = "vscode-yaml";
+        publisher = "redhat";
+        version = "1.14.0";
+        sha256 = "sha256-hCRyDA6oZF7hJv0YmbNG3S2XPtNbyxX1j3qL1ixOnF8=";
+      }];
+    })
+  ];
+}
