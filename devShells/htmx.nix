7a8ff9a0fa138e8c4d3e8cb2d72cd4ba908d92c3
GetPsyched <priyanshu@getpsyched.dev>
2024-01-10 18:00:16 +0530

    feat: htmx

---
+++ devShells/htmx.nix
@@ -0,0 +1,12 @@
+{ pkgs }:
+{
+  default = with pkgs; [ htmx-lsp ];
+  vscodium = {
+    extensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+      name = "htmx-tags";
+      publisher = "otovo-oss";
+      version = "0.0.8";
+      hash = "sha256-sF5VpdmPluygAiGt9a9E/bM/VzA6a++0dR87dweMCyQ=";
+    }];
+  };
+}
